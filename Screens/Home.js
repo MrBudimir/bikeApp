5ec84afd3d75ec701eeec658d50a6bc6aa536796
IB <ivica.budimir@campus02.edu.at>
2021-04-15 16:58:25 +0200

    WIP: add map view
    add map view in home screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,73 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { Ionicons } from "@expo/vector-icons";
+import Carousel from "react-native-snap-carousel";
+
+//47.07254033769078, 15.438058106976376
+const mapData = require("./MapData/Map");
+
+const handlePress = () => {
+  console.log("pressed");
+};
+
+const renderCarouselItem = ({ item }) => {
+  return (
+    <View style={styles.cardContainer}>
+      <Text style={styles.title}>{item.name}</Text>
+    </View>
+  );
+};
+
+const onCarouselItemChange = (index) => {
+  let marker = mapData.state.markers[index];
+
+  _map.animateToRegion({
+    latitude: marker.coordinate.latitude,
+    longitude: marker.coordinate.longitude,
+    latitudeDelta: 0.04864195044303443,
+    longitudeDelta: 0.040142817690068,
+  });
+};
+
+const onMarkerPressed = (index) => {
+  this._carousel.snapToItem(index);
+};
 
 const HomeScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>HomeScreen</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={mapData.state.region}
+        customMapStyle={mapData.generatedMapStyle}
+        ref={(ref) => (_map = ref)}
+      >
+        {mapData.state.markers.map((marker, index) => {
+          return (
+            <Marker
+              key={index}
+              coordinate={marker.coordinate}
+              onPress={onMarkerPressed(index)}
+              ref={(ref) => {
+                marker = ref;
+              }}
+            >
+              <Text>{marker.name}</Text>
+              <Ionicons name="bicycle" size={40} color="#F2AA4CFF" />
+            </Marker>
+          );
+        })}
+      </MapView>
+      <Carousel
+        ref={(ref) => (this._carousel = ref)}
+        data={mapData.state.markers}
+        sliderWidth={300}
+        itemWidth={300}
+        renderItem={renderCarouselItem}
+        containerCustomStyle={styles.carousel}
+        onSnapToItem={(index) => onCarouselItemChange(index)}
+      />
     </View>
   );
 };
@@ -12,6 +75,27 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  carousel: {
+    position: "absolute",
+    bottom: 0,
+    left: "25%",
+    right: "25%",
+    marginBottom: 25,
+  },
+  cardContainer: {
+    backgroundColor: "#101820FF",
+    height: 125,
+    width: 225,
+    borderRadius: 15,
+    opacity: 0.9,
+    alignItems: "center",
+  },
+  title: {
+    color: "#F2AA4CFF",
   },
 });
 
---
+++ Screens/MapData/Map.js
@@ -0,0 +1,337 @@
+export const generatedMapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#8ec3b9",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1a3646",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4b6878",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#64779e",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4b6878",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#334e87",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#283d6a",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6f9ba5",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#3C7680",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#304a7d",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#98a5be",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2c6675",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#255763",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#b0d5ce",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#98a5be",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "transit.line",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#283d6a",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#3a4762",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#0e1626",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#4e6d70",
+      },
+    ],
+  },
+];
+
+export const state = {
+  markers: [
+    {
+      coordinate: {
+        latitude: 47.07254033769078,
+        longitude: 15.438058106976376,
+      },
+      name: "Station A",
+      description: "This is the best place in Portland",
+    },
+    {
+      coordinate: {
+        latitude: 47.06746938238355,
+        longitude: 15.442514011841658,
+      },
+      name: "Station B",
+      description: "This is the second best place in Portland",
+    },
+    {
+      coordinate: {
+        latitude: 47.0860003268991,
+        longitude: 15.440071669513575,
+      },
+      name: "Station C",
+      description: "This is the third best place in Portland",
+    },
+    {
+      coordinate: {
+        latitude: 47.04098737675895,
+        longitude: 15.46298449834799,
+      },
+      name: "Station D",
+      description: "This is the fourth best place in Portland",
+    },
+  ],
+  region: {
+    latitude: 47.07254033769078,
+    longitude: 15.438058106976376,
+    latitudeDelta: 0.04864195044303443,
+    longitudeDelta: 0.040142817690068,
+  },
+};
